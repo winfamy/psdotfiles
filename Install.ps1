ba9ef73cac33b0295556664dee96540a9b79f24a
Grady Phillips <grady@unyx.io>
2023-07-28 11:49:51 -0700

    Add files via upload

---
+++ Install.ps1
@@ -0,0 +1,31 @@
+# build local modules dir if not exist
+$localPSModulePath = [IO.Path]::Combine($HOME, "Documents", "WindowsPowerShell", "Modules");
+# assigning to $null silences the output
+$null = New-Item -ItemType Directory -Force -Path $localPSModulePath
+
+# build path to copy modules directory
+$modulesPath = [IO.Path]::Combine($PSScriptRoot, "Modules")
+$modulesStar = -join ($modulesPath, "\*");
+Copy-Item -Force -Recurse $modulesStar $localPSModulePath
+Copy-Item -Force ([IO.Path]::Combine($PSScriptRoot, "Profile.ps1")) $profile
+
+# import modules from local module lib
+Import-Module EnvPathFunctions
+
+
+$pathsToAddToPath = @(
+    Join-Path $PSScriptRoot "Path"
+);
+
+foreach ($path in $pathsToAddToPath) {
+    if ( !(Get-DoesExistInEnvPath $path) ) {
+        Write-Output "$path added to PATH"
+        Add-EnvPath $path
+    }
+}
+
+
+# bitwarden
+$pwd = Read-Host "Bitwarden Master Password" -AsSecureString
+$pwd_plaintext = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pwd))
+[Environment]::SetEnvironmentVariable("BW_PW", $pwd_plaintext, "User")
---
+++ Profile.ps1
@@ -0,0 +1,16 @@
+# Author: 2d Lt Grady Phillips
+# Grady's Powershell Profile
+
+$ErrorActionPreference= 'silentlycontinue'
+
+# this allows bitwarden to connect via API even tho SSL connections dont work right
+$env:NODE_TLS_REJECT_UNAUTHORIZED = 0
+$env:NODE_NO_WARNINGS = 1
+
+$bw_email = "grady@unyx.io"
+$session_key = bw unlock --passwordenv "BW_PW" --raw
+$env:BW_SESSION = $session_key
+
+Import-Module Discord -DisableNameChecking
+
+Set-Alias -Name "disc" -Value Launch-Discord
